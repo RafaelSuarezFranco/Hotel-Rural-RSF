b94de409ca9ac44813b4fa3396a2cead1a1ef163
Rafael Suárez Franco <cientoveintisiete127@gmail.com>
2022-04-24 12:24:45 +0200

    recuperar contraseña

--- Unit19.pas
+++ Unit19.pas
@@ -4,8 +4,11 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
-  Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,   System.Math,
+  Vcl.StdCtrls, IdTCPConnection, IdTCPClient, IdExplicitTLSClientServerBase,
+  IdMessageClient, IdSMTPBase, IdSMTP, IdComponent, IdIOHandler,     IdEMailAddress, IdGlobal, IdAttachmentFile,
+  IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL, IdBaseComponent,
+  IdMessage;
 
 type
   TLogin = class(TForm)
@@ -20,6 +23,9 @@ type
     Button2: TButton;
     Label5: TLabel;
     Label6: TLabel;
+    IdMessage1: TIdMessage;
+    IdSSLIOHandlerSocketOpenSSL1: TIdSSLIOHandlerSocketOpenSSL;
+    IdSMTP1: TIdSMTP;
     procedure FormActivate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
@@ -27,6 +33,7 @@ type
     procedure Label5MouseLeave(Sender: TObject);
     procedure FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure Label5Click(Sender: TObject);
+    procedure Label6Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -159,8 +166,103 @@ begin
   AltaUsuario.showmodal;
 end;
 
+//recuperar contraseña
+ procedure TLogin.Label6Click(Sender: TObject);
+ var
+ codigoVerificacion: string;
+ codigoInput: string;
+ correo: string;
+ pswd1: string;
+ pswd2: string;
+ usuario : string;
+begin
 
 
+ IdMessage1.Clear;
+
+// IO HANDLER SETTINGS //
+  With IdSSLIOHandlerSocketOpenSSL1 do
+  begin
+
+    Destination := 'smtp.gmail.com:587';
+    Host := 'smtp.gmail.com';
+    MaxLineAction := maException;
+    Port := 587;
+    SSLOptions.Method := sslvTLSv1;
+    SSLOptions.Mode := sslmUnassigned;
+    SSLOptions.VerifyMode := [];
+    SSLOptions.VerifyDepth := 0;
+  end;
+//SETTING SMTP COMPONENT DATA //
+  IdSMTP1.Host := 'smtp.gmail.com';
+  IdSMTP1.Port := 587;
+  IdSMTP1.Username := 'hotelrafaelsuarezfranco@gmail.com'; // please change to your gmail address //
+  IdSMTP1.Password := 'hotelrsf127';
+  IdSMTP1.IOHandler := IdSSLIOHandlerSocketOpenSSL1;
+  //IdSMTP1.AuthType := satDefault;
+  IdSMTP1.UseTLS := utUseExplicitTLS;
+
+
+
+  codigoVerificacion := inttostr(RandomRange(100000, 999999));
+  correo := inputbox('Introduzca su correo', 'Correo de la cuenta que desea recuperar:', '');
+  if Tablas.FDTableUsuarios.Locate('correo', correo, []) then
+    begin
+
+      usuario := Tablas.FDTableUsuariosusuario.Value;
+
+      IdMessage1.From.Address :=  'hotelrafaelsuarezfranco@gmail.com';
+      IdMessage1.Recipients.EMailAddresses := correo;
+      IdMessage1.Subject := 'Recuperar cuenta de Hotel RSF';
+      IdMessage1.Body.Text := 'Buenos días, '+usuario+'. Su código de recuperación es: '+codigoVerificacion;
+
+       TRY
+          IdSMTP1.Connect();
+          IdSMTP1.Send(IdMessage1);
+          IdSMTP1.Disconnect();
+       except on e:Exception do
+          begin
+            ShowMessage(e.Message);
+            IdSMTP1.Disconnect();
+          end;
+       END;
+
+      while codigoInput <> codigoVerificacion do
+        begin
+          codigoInput := inputbox('Introduzca el código de verificación', 'Hemos mandado un código a su correo.', '');
+        end;
+
+      if codigoinput = codigoVerificacion then
+        begin
+          while pswd1 = '' do
+            begin
+              pswd1 := inputbox('Nueva contraseña', #31'Introduzca la nueva contraseña.', '');
+            end;
+          while pswd1 <> pswd2 do
+            begin
+              pswd2 := inputbox('Repita contraseña', #31'Repita la nueva contraseña.', '');
+            end;
+
+          if pswd1 = pswd2 then
+            begin
+
+              Tablas.FDTableUsuarios.Locate('correo', correo, []);
+              Tablas.FDTableUsuarios.Edit;
+              Tablas.FDTableUsuariospassword.Value := Tablas.passwordHash(pswd1);
+              Tablas.FDTableUsuarios.Post;
+              showmessage('Contraseña cambiada con éxito');
+            end;
+
+        end;
+
+
+    end else
+    begin
+      showmessage('No existe una cuenta con el correo introducido.');
+    end;
+
+
+end;
 
 
 
@@ -176,6 +278,8 @@ procedure TLogin.Label5MouseLeave(Sender: TObject);
 begin
   TLabel(sender).StyleElements :=[seFont,seClient,seBorder];
 end;
+
+
 
 
 procedure TLogin.FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
